eb11ace6238bcbacb38dc289a5946def6d60e0d7
Miriam Gonçalves <miriam.cristiana.goncalves@blip.pt>
2019-05-05 17:59:55 +0100

    Updates

---
+++ precache-manifest.caea5da200543d3008d5ea43cc52b783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ce5a23b74a811110a1a47e1ded257e",
+    "url": "/flickr-react/index.html"
+  },
+  {
+    "revision": "6fb5c2237012f80e9d74",
+    "url": "/flickr-react/static/css/main.2597becc.chunk.css"
+  },
+  {
+    "revision": "1eb8a322653916d9bb70",
+    "url": "/flickr-react/static/js/2.e98fd868.chunk.js"
+  },
+  {
+    "revision": "6fb5c2237012f80e9d74",
+    "url": "/flickr-react/static/js/main.1cd91bf9.chunk.js"
+  },
+  {
+    "revision": "bde4d995d6654b3773e3",
+    "url": "/flickr-react/static/js/runtime~main.0942ef0f.js"
+  }
+]);
